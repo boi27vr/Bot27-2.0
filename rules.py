c9560449eb418f714e06722589d83d2bb2c8764a
boi27vr <boi27.i.like.playing.vr@gmail.com>
2026-08-02 16:31:22 -0500

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,25 @@
+RULES = {
+    1: ("Absolutely no hate speech, toxicity, or bullying! Any form of hate, "
+        "including, but not limited to, homophobia, transphobia, xenophobia, "
+        "sexism, or racism will instantly result in a permanent ban!"),
+    2: ("No offensive content. Dark humor is allowed but make sure nobody "
+        "gets offended."),
+    3: ("Be respectful. You don't have to be all sunshine and rainbows but "
+        "no harassment, over the top swearing, or personal attacks.\n"
+        "✅ \"What the ####\"\n"
+        "❌ \"Shut the #### up\""),
+    4: ("No inappropriate content! It doesn't have to be exactly family "
+        "friendly, but don't act crazy. NSFW of any kind is not allowed. "
+        "This includes, but is not limited to, nudity, extreme gore, "
+        "excessive horror, etc."),
+    5: "Follow Discord's Terms of Service.",
+    6: ("Use channels properly. No spam — we have meme and spam channels "
+        "for that. Also, please stay on topic. There is an off-topic channel."),
+    7: ("Listen to staff. In order to keep a friendly and safe environment, "
+        "please listen to staff and do what they say."),
+    8: ("No impersonating. This includes staff, helpers, or anyone else "
+        "in charge."),
+    9: "Keep drama out. It is unbelievably annoying...",
+    10: ("Have common sense. If it feels like it might cause problems, "
+         "just don't do it!"),
+}
